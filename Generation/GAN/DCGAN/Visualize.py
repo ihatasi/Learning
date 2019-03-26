eb564a3f87fde81a08d34d95b7734c1b39d6758d
ihatasi <ihatasi@gmail.com>
2019-03-26 18:55:22 +0900

    for Mnist

--- Generation/GAN/DCGAN/Visualize.py
+++ Generation/GAN/DCGAN/Visualize.py
@@ -23,13 +23,15 @@ def out_generated_image(gen, dis, rows, cols, seed, dst):
 
         x = np.asarray(np.clip(x * 255, 0.0, 255.0), dtype=np.uint8)
         _, _, H, W = x.shape
-        x = x.reshape((rows, cols, 3, H, W))
+        #x = x.reshape((rows, cols, 3, H, W))
+        x = x.reshape((rows, cols, 1, H, W))
         x = x.transpose(0, 3, 1, 4, 2)
-        x = x.reshape((rows * H, cols * W, 3))
+        #x = x.reshape((rows * H, cols * W, 3))
+        x = x.reshape((rows * H, cols * W))
 
         preview_dir = '{}/preview'.format(dst)
         preview_path = preview_dir +\
-            '/image{:0>8}.png'.format(trainer.updater.iteration)
+            '/image{:0>8}.png'.format(trainer.updater.epoch)
         if not os.path.exists(preview_dir):
             os.makedirs(preview_dir)
         Image.fromarray(x).save(preview_path)
